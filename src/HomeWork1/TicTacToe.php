eb5b1eebe027be212a06d5c0b6eef97b1bab4ae2
Evgeniy Fedyushkin <esfedyushkin@avito.ru>
2019-11-13 19:12:25 +0300

    tictactoe task

--- src/HomeWork1/TicTacToe.php
+++ src/HomeWork1/TicTacToe.php
@@ -4,82 +4,61 @@ namespace HomeWork1;
 
 class TicTacToe
 {
-    private const win = 3;
-
-    private $arr;
-
-    public function check($arr)
+    public function check($playingField)
     {
-        $this->arr = $arr;
+        [$horizontals, $verticals, $diagonals] = $this->constructCombinations($playingField);
+        $horizontalWinner = $this->isSame($horizontals);
+        $verticalWinner = $this->isSame($verticals);
+        $diagonalWinner = $this->isSame($diagonals);
 
-        $result = [
-            $this->checkHorizons(),
-            $this->checkVerticals(),
-            $this->checkDiagonals(),
+        if ($horizontalWinner) return $horizontalWinner[0];
+        if ($verticalWinner) return $verticalWinner[0];
+        if ($diagonalWinner) return $diagonalWinner[0];
+
+        return false;
+    }
+
+    public function constructCombinations($playingField)
+    {
+        $horizontals = [];
+        $verticals = [
+            [],
+            [],
+            [],
+        ];
+        $diagonals = [
+            [],
+            []
         ];
 
-        $result = array_filter($result);
-        return array_pop($result) ?? false;
-    }
+        foreach ($playingField as $y => $horizontal) {
+            $horizontals[] = $horizontal;
 
-    public function checkHorizons()
-    {
-        $result = [0, 0, 0];
+            foreach ($horizontal as $x => $sign) {
+                $verticals[$x][$y][] = $sign;
 
-        foreach ($this->arr as $key => $line) {
-            $first = array_filter($line)[0] ?? null;
-            foreach ($line as $element) {
-                if ($element == $first) {
-                    $result[$key]++;
+                if ($x === $y) {
+                    $diagonals[0][] = $sign;
+
+                    if ($x === 1) {
+                        $diagonals[1][] = $sign;
+                    }
+                }
+
+                if (($x === 0 && $y === 2) || (($x === 2 && $y === 0))) {
+                    $diagonals[1][] = $sign;
                 }
             }
         }
 
-        if (in_array(self::win, $result)) {
-            $winner = array_search(self::win, $result);
-            return $this->arr[$winner][0];
-        }
-
-        return false;
+        return [$horizontals, $verticals, $diagonals];
     }
 
-    private function checkVerticals()
-    {
-        $result = [0, 0, 0];
-
-        for ($y = 0; $y < self::win; $y++) {
-            for ($x = 0; $x < self::win; $x++) {
-                if ($this->arr[$y][$x] == $this->arr[$x][0]) {
-                    $result[$x]++;
-                }
+    public function isSame($combinations) {
+        foreach ($combinations as $combination) {
+            if ($combination[0] === $combination[1] && $combination[0] === $combination[2]) {
+                return $combination;
             }
-        }
-
-        if (in_array(self::win, $result)) {
-            $winner = array_search(self::win, $result);
-            return $this->arr[0][$winner];
-        }
-
-        return false;
-    }
-
-    private function checkDiagonals()
-    {
-        $result = [0, null, 0];
-
-        for ($y = 0; $y < self::win; $y++) {
-            if ($this->arr[$y][$y] == $this->arr[0][0]) {
-                $result[0]++;
-            }
-
-            if ($this->arr[$y][self::win - 1 - $y] == $this->arr[0][2]) {
-                $result[2]++;
-            }
-        }
-
-        if (in_array(self::win, $result)) {
-            $winner = array_search(self::win, $result);
-            return $this->arr[0][$winner];
         }
 
         return false;
